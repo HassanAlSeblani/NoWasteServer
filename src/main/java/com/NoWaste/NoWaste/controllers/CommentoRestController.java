a6426e8bd75dad02ca56ce38c2bdd86a08d86bd5
MERENDRAKE96 <raffaelesparaco96@gmail.com>
2024-03-25 17:29:14 +0100

    commento service,controller e dao

--- src/main/java/com/NoWaste/NoWaste/controllers/CommentoRestController.java
+++ src/main/java/com/NoWaste/NoWaste/controllers/CommentoRestController.java
@@ -1,5 +1,82 @@
 package com.NoWaste.NoWaste.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.NoWaste.NoWaste.entities.Commento;
+import com.NoWaste.NoWaste.services.CommentoService;
+
+@CrossOrigin(origins = { "http://localhost:4200" })
+@RestController
+@RequestMapping("/commenti")
 public class CommentoRestController {
-    
+
+    @Autowired
+    private CommentoService commentoService;
+
+    @GetMapping("/commentsByUser")
+    public List<Commento> getCommentiByUser(@RequestHeader("token") String token, @RequestParam("userId") int userId) {
+        if (token != null) {
+            if (!token.equalsIgnoreCase("") && !token.split("-")[0].equalsIgnoreCase("NONE")) {
+                return commentoService.getCommentiByUser(userId);
+            }
+        }
+        return null;
+    }
+
+    @GetMapping("/commentsByRecipe")
+    public List<Commento> getCommentiByRecipe(@RequestHeader("token") String token,
+            @RequestParam("recipeId") int recipeId) {
+
+        if (token != null) {
+            if (!token.equalsIgnoreCase("") && !token.split("-")[0].equalsIgnoreCase("NONE")) {
+                return commentoService.getCommentiByRecipe(recipeId);
+            }
+        }
+        return null;
+    }
+
+    @PostMapping("/deleteComment")
+    public boolean deleteCommento(@RequestHeader("token") String token, @RequestParam("commentId") int commentId) {
+
+        if (token != null) {
+            if (!token.equalsIgnoreCase("") && !token.split("-")[0].equalsIgnoreCase("NONE")) {
+                return commentoService.deleteCommento(commentId);
+            }
+        }
+        return false;
+    }
+
+    @PostMapping("/updateComment")
+    public boolean updateCommento(@RequestHeader("token") String token, @RequestBody Commento commento) {
+
+        if (token != null) {
+            if (!token.equalsIgnoreCase("") && !token.split("-")[0].equalsIgnoreCase("NONE")) {
+                return commentoService.updateCommento(commento);
+            }
+        }
+        return false;
+    }
+
+    @GetMapping("/allComments")
+    public List<Commento> getAllCommenti(@RequestHeader("token") String token) {
+
+        if (token != null) {
+            if (!token.equalsIgnoreCase("") && !token.split("-")[0].equalsIgnoreCase("NONE")) {
+                return commentoService.getAllCommenti();
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/com/NoWaste/NoWaste/services/CommentoService.java
+++ src/main/java/com/NoWaste/NoWaste/services/CommentoService.java
@@ -1,5 +1,52 @@
 package com.NoWaste.NoWaste.services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.NoWaste.NoWaste.DAO.CommentoDAO;
+import com.NoWaste.NoWaste.entities.Commento;
+import com.NoWaste.NoWaste.entities.Entity;
+
+@Service
 public class CommentoService {
+
+    @Autowired
+    private CommentoDAO commentoDAO;
+
+    public boolean createCommento(Commento commento) {
+        return commentoDAO.create(commento);
+    }
+
+    public List<Commento> getAllCommenti() {
+        List <Commento> ris = new ArrayList<Commento>();
+        for(Entity e : commentoDAO.read().values()){
+            ris.add((Commento) e);
+        }
+        return ris;
+    }
+
+    public boolean updateCommento(Commento commento) {
+        return commentoDAO.update(commento);
+    }
+
+    public boolean deleteCommento(int id) {
+        return commentoDAO.delete(id);
+    }
+
+    public Commento getCommentoById(int id) {
+        return (Commento) commentoDAO.readById(id);
+    }
+
+    public List<Commento> getCommentiByUser(int userId) {
+        return commentoDAO.getCommentoByUser(userId);
+    }
+
+    public List<Commento> getCommentiByRecipe(int recipeId) {
+        return commentoDAO.getCommentoByRecipe(recipeId);
+    }
     
 }
